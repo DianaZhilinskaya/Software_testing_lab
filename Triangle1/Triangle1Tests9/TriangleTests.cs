b94a48f34247582a17d6d3081c3db8d702a8b961
Diana Zhilinskaya <zilinskaadiana7@gmail.com>
2018-09-28 11:24:53 +0300

    lab3 unit test 9,10

---
+++ Triangle1/Triangle1Tests9/TriangleTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle1.Tests
+{
+    [TestClass()]
+    public class TriangleTests
+    {
+        [TestMethod()]
+        public void test_c_minus_b_minus_a_minus_returnfalse()
+        {
+            int a = -5;
+            int b = -3;
+            int c = -5;
+            bool s = false;
+
+            Triangle second = new Triangle();
+            bool actual = second.test(a, b, c);
+
+            Assert.AreEqual(s, actual);
+        }
+    }
+}

73053e00b87e26e13b96c1b40ce8bfe27ca0c330
Diana Zhilinskaya <zilinskaadiana7@gmail.com>
2018-10-05 09:47:36 +0300

    lab3 correct option

---
+++ Triangle2/Triangle2/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle2
+{
+    public class Triangle
+    {
+        public bool test(int a, int b, int c)
+        {
+            if (a > 0 && b > 0 && c > 0)
+            {
+                if (a + b <= c || a + c <= b || b + c <= a)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            else { Console.WriteLine("This can not be."); return false; }
+        }
+    }
+}
